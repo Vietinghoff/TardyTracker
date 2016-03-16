19d92a4d7592c5cbffb455a712eef7a84c1bdb49
Justin Vietinghoff <tall.guy2010@hotmail.com>
2016-03-15 23:26:38 -0400

    add in checkStudentTardys

---
+++ TardyTracker/TardyTracker/checkStudentTardys.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TardyTracker
+{
+    class checkStudentTardys
+    {
+        public bool checkTardyNumber(string studentId)
+        {
+            string studentID = studentId;
+
+
+        }
+    }
+}
