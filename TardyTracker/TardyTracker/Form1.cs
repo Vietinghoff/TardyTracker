93cef04c2a2cfbc5729daf697889217b9b0c1050
Justin Vietinghoff <tall.guy2010@hotmail.com>
2016-03-15 22:16:22 -0400

    Initial Commit

---
+++ TardyTracker/TardyTracker/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TardyTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            DataTable dataTable = new DataTable();
+
+            string idNumber = tbIdNumber.Text;
+            getDataConnection dataConnection = new getDataConnection();
+
+            dataConnection.dataCmdString("UPDATE [SHEET1$] SET Tardys = Tardys + 1 WHERE Id = " + idNumber);
+
+            dataTable = dataConnection.dataTableFill(idNumber);
+
+            dataView.DataSource = dataTable;
+        }
+    }
+}
---
+++ TardyTracker/TardyTracker/getDataConnection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+using System.Windows;
+
+
+namespace TardyTracker
+{
+    public class getDataConnection
+    {
+        public DataTable dataTableFill(string idNumber)
+        {
+
+            string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data source=" + @"C:\GitHubRepos\TardyTracker\TardyTracker\Tardy Tracker Spread Sheet.xls"
+                + ";" + "Extended Properties=Excel 8.0;";
+
+            OleDbConnection objConnection = new OleDbConnection(connectionString);
+
+            objConnection.Open();
+            string qry = "SELECT * FROM [Sheet1$] WHERE id = " + idNumber;
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(qry, objConnection);
+            da.Fill(dt);
+            objConnection.Close();
+            da.Dispose();
+            return dt;
+        }
+
+        public void dataTableSave(DataTable dataTable)
+        {
+            DataTable dt = new DataTable();
+            dt = dataTable;
+            string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data source=" + @"C:\GitHubRepos\TardyTracker\TardyTracker\Tardy Tracker Spread Sheet.xls"
+                + ";" + "Extended Properties=Excel 8.0;";
+
+            OleDbConnection objConnection = new OleDbConnection(connectionString);
+
+            objConnection.Open();
+            string qry = "UPDATE [Sheet1$] FROM " + dt;
+            OleDbCommand cmd = new OleDbCommand(qry, objConnection);
+            cmd.ExecuteNonQuery();
+            objConnection.Close();
+
+        }
+
+        public void dataCmdString(string qry)
+        {
+            string command = qry;
+
+            string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data source=" + @"C:\GitHubRepos\TardyTracker\TardyTracker\Tardy Tracker Spread Sheet.xls"
+                + ";" + "Extended Properties=Excel 8.0;";
+
+            OleDbConnection objConnection = new OleDbConnection(connectionString);
+            objConnection.Open();
+
+            OleDbCommand cmd = new OleDbCommand(command, objConnection);
+            cmd.ExecuteNonQuery();
+            objConnection.Close();
+        }
+    }
+
+
+}
+
